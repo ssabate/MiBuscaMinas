785a4dd0319aded1d54df8ae179213ad3b789497
Santi Sabate <ssabate@iesebre.com>
2018-04-09 09:42:39 +0200

    Finalitzat sense fitxers i en OO

---
+++ src/mibuscaminas/Casella.java
@@ -0,0 +1,228 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mibuscaminas;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.Rectangle2D;
+import javax.swing.ImageIcon;
+
+/**
+ * Classe que representa les caselles del taulell
+ *
+ * @author profe
+ */
+public class Casella extends Rectangle2D.Float {
+    
+    private Image img;                  //imatge que posem a la casella
+    private float x;                    //coordenada x de la casella
+    private float y;                    //coordenada x de la casella
+    private Estat estat;                //estat en el que es troba la casella
+    private boolean clicada = false;    //per saber si l'han clicat
+    private boolean marcada = false;    //per saber si l'han marcat
+
+    //Creem la casella posant un requadre i l'estat a CERO
+    public Casella(float xpos, float ypos, float width, float height) {
+        x = xpos;
+        y = ypos;
+        setRect(x, y, width, height);
+        estat = Estat.CERO;
+    }
+
+    //Creem la casella assignant imatge, que fixa també el tamany    
+    public Casella(float x, float y, Image imagen) {
+        this(x, y, imagen.getWidth(null), imagen.getHeight(null));
+        img = imagen;
+    }
+    
+    /**
+     * @return the img
+     */
+    public Image getImg() {
+        return img;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the estat
+     */
+    public Estat getEstat() {
+        return estat;
+    }
+
+    /**
+     * @param estat the estat to set
+     */
+    public void setEstat(Estat estat) {
+        this.estat = estat;
+    }
+
+    /**
+     * @return the clicada
+     */
+    public boolean isClicada() {
+        return clicada;
+    }
+
+    /**
+     * @param clicada the clicada to set
+     */
+    public void setClicada(boolean clicada) {
+        this.clicada = clicada;
+    }
+
+    /**
+     * @return the marcada
+     */
+    public boolean isMarcada() {
+        return marcada;
+    }
+
+    /**
+     * @param marcada the marcada to set
+     */
+    public void setMarcada(boolean marcada) {
+        this.marcada = marcada;
+    }
+
+    
+
+    //Dibuixem la casella juntant la forma en la imatge que conté la seua propietat imatge
+    public void dibuja(Graphics2D g) {
+        g.fill(this);
+        g.drawImage(getImg(), (int) x, (int) y, null);
+    }
+
+    //Retornem una nova casella en les mateixes propietats que la actual però situada unes caselles més avall 
+    public Casella copia(int numcas) {
+        return new Casella(this.x, this.y + getImg().getHeight(null) * numcas, this.getImg());
+    }
+
+    //Dibuixem la casella juntant la forma en la imatge que passem com a paràmetre
+    public void dibuja(Graphics2D g, int numcas, Image imagen) {
+        Casella copia = this.copia(numcas);
+        g.fill(copia);
+        g.drawImage(imagen, (int) copia.x, (int) copia.y, null);
+    }
+
+    //Indica si les coordenades x i y formen part de la casella
+    public boolean clicada(float x, float y) {
+        return getBounds2D().contains(x, y);
+    }
+
+    public void setImg(Image img) {
+        this.img = img;
+    }
+
+    //Retornem la imatge que representa l'estat de la casella
+    public Image buscaImagen() {
+        Image mshi = null;
+        switch (this.getEstat()) {
+            case CERO:
+                mshi = new ImageIcon("src/imagenes/0.png").getImage();
+                break;
+            case UNO:
+                mshi = new ImageIcon("src/imagenes/1.png").getImage();
+                break;
+            case DOS:
+                mshi = new ImageIcon("src/imagenes/2.png").getImage();
+                break;
+            case TRES:
+                mshi = new ImageIcon("src/imagenes/3.png").getImage();
+                break;
+            case CUATRO:
+                mshi = new ImageIcon("src/imagenes/4.png").getImage();
+                break;
+            case CINCO:
+                mshi = new ImageIcon("src/imagenes/5.png").getImage();
+                break;
+            case SEIS:
+                mshi = new ImageIcon("src/imagenes/6.png").getImage();
+                break;
+            case SIETE:
+                mshi = new ImageIcon("src/imagenes/7.png").getImage();
+                break;
+            case OCHO:
+                mshi = new ImageIcon("src/imagenes/8.png").getImage();
+                break;
+            case BOMBA:
+                mshi = new ImageIcon("src/imagenes/9.png").getImage();
+                break;
+        }
+        return mshi;
+    }
+    
+    public void incrementaEstat(){
+        //Passem de l'estat actual de la casella al següent
+        estat = estat.sumarUn();
+    }
+}
+
+//Diferents estats en que es poden trobar les caselles del taulell
+enum Estat {
+    BOMBA(-1),
+    CERO(0),
+    UNO(1),
+    DOS(2),
+    TRES(3),
+    CUATRO(4),
+    CINCO(5),
+    SEIS(6),
+    SIETE(7),
+    OCHO(8);
+
+    private final int valor;
+
+    Estat(int v) {
+        valor = v;
+    }
+
+    //Passem d'un estat al següent (sumem 1)
+    public Estat sumarUn() {
+        Estat seg = this;
+        switch (valor) {
+            case 0:
+                seg = UNO;
+                break;
+            case 1:
+                seg = DOS;
+                break;
+            case 2:
+                seg = TRES;
+                break;
+            case 3:
+                seg = CUATRO;
+                break;
+            case 4:
+                seg = CINCO;
+                break;
+            case 5:
+                seg = SEIS;
+                break;
+            case 6:
+                seg = SIETE;
+                break;
+            case 7:
+                seg = OCHO;
+                break;
+        }
+        return seg;
+    }
+
+}
--- src/mibuscaminas/Tablero.java
+++ src/mibuscaminas/Tablero.java
@@ -10,69 +10,13 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.MouseInfo;
 import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
-//Diferents estats en que es poden trobar les caselles del taulell
-enum Estat {
-    BOMBA(-1),
-    CERO(0),
-    UNO(1),
-    DOS(2),
-    TRES(3),
-    CUATRO(4),
-    CINCO(5),
-    SEIS(6),
-    SIETE(7),
-    OCHO(8);
-
-    private final int valor;
-
-    Estat(int v) {
-        valor = v;
-    }
-
-    //Passem d'un estat al següent (sumem 1)
-    public Estat sumarUn() {
-        Estat seg = this;
-        switch (valor) {
-            case 0:
-                seg = UNO;
-                break;
-            case 1:
-                seg = DOS;
-                break;
-            case 2:
-                seg = TRES;
-                break;
-            case 3:
-                seg = CUATRO;
-                break;
-            case 4:
-                seg = CINCO;
-                break;
-            case 5:
-                seg = SEIS;
-                break;
-            case 6:
-                seg = SIETE;
-                break;
-            case 7:
-                seg = OCHO;
-                break;
-        }
-        return seg;
-    }
-
-}
 
 /**
  *
@@ -105,7 +49,7 @@ public class Tablero extends JPanel {
     }
 
     //Mètode que conté el codi per tractar les accions fetes en el ratolí
-    public void accionsRatoli() {
+    private void accionsRatoli() {
 
         MouseAdapter mouseAdapter = new MouseAdapter() {
             //Quan fem clic en el ratolí sobre el taulell s'executarà el següent codi
@@ -141,11 +85,11 @@ public class Tablero extends JPanel {
                 //Si han apretado el botón izquierdo
                 if (e.getButton() == MouseEvent.BUTTON1) {
                     //Si hem seleccionat casella, i no está marcada ni clicada actuem en el clic
-                    if (seleccionat != null && !seleccionat.marcada && !seleccionat.clicada) {
+                    if (seleccionat != null && !seleccionat.isMarcada() && !seleccionat.isClicada()) {
                         //Incrementem el comptador de caselles clicades
                         contClicadas++;
                         //Mirem què hi ha a la casella i actuem en conseqüència
-                        switch (seleccionat.estat) {
+                        switch (seleccionat.getEstat()) {
                             case BOMBA:
                                 //Si hi havia una bomba el joc s'ha acabat i hem perdut
                                 gameOver = true;
@@ -153,7 +97,7 @@ public class Tablero extends JPanel {
                             default:
                                 //Si no marquem la casella com a clicada i li posem la imatge 
                                 //correponent al seu estat
-                                seleccionat.clicada = true;
+                                seleccionat.setClicada(true);
                                 seleccionat.setImg(seleccionat.buscaImagen());
                                 //Mirem si s'han de mostrar els veïns 
                                 Tablero.actualitzarVeins(i, j);
@@ -169,20 +113,20 @@ public class Tablero extends JPanel {
                 } else //Si han apretado el botón derecho --> per marcar la casella com a que té una bomba
                 if (e.getButton() == MouseEvent.BUTTON3) {
                     //Si s'ha seleccionat alguna casella no clicada actuem
-                    if (seleccionat != null && !seleccionat.clicada) {
+                    if (seleccionat != null && !seleccionat.isClicada()) {
                         //Si intentem marcar una quant ja les haviem marcat totes no ho dixem fer
-                        if (!seleccionat.marcada && contMarcadas == NUM_BOMBAS) {
+                        if (!seleccionat.isMarcada() && contMarcadas == NUM_BOMBAS) {
                             return;
                         }
                         //Marquem o desmarquem la casella
-                        seleccionat.marcada = !seleccionat.marcada;
+                        seleccionat.setMarcada(!seleccionat.isMarcada());
                         //Posem la imatge de marcada o desmarcada
-                        img = (seleccionat.marcada ? new ImageIcon("src/imagenes/11.png").getImage() : new ImageIcon("src/imagenes/10.png").getImage());
+                        img = (seleccionat.isMarcada() ? new ImageIcon("src/imagenes/11.png").getImage() : new ImageIcon("src/imagenes/10.png").getImage());
                         seleccionat.setImg(img);
                         //Incrementem o decrementem els comptadors de marcades totals i correctes 
-                        contMarcadas += (seleccionat.marcada ? 1 : -1);
-                        if (seleccionat.estat == Estat.BOMBA) {
-                            contMarcadasCorrectas += (seleccionat.marcada ? 1 : -1);
+                        contMarcadas += (seleccionat.isMarcada() ? 1 : -1);
+                        if (seleccionat.getEstat() == Estat.BOMBA) {
+                            contMarcadasCorrectas += (seleccionat.isMarcada() ? 1 : -1);
                         }
                         //Si és l'última marcada i totes les caselles en bomba són correctes el joc s'ha acabat
                         //i hem guanyat
@@ -234,8 +178,8 @@ public class Tablero extends JPanel {
             int i = r.nextInt(DIM);
             int j = r.nextInt(DIM);
             //Només tractarem la casella posant una bomba si no tenia ja una abans 
-            if (tablero[i][j].estat != Estat.BOMBA) {
-                tablero[i][j].estat = Estat.BOMBA;
+            if (tablero[i][j].getEstat() != Estat.BOMBA) {
+                tablero[i][j].setEstat(Estat.BOMBA);
                 //Quan posem una bomba a una casella hem d'actualitzar l'estat de les que l'envolten
                 actualitzarCaselles(i, j);
                 k++;
@@ -246,12 +190,12 @@ public class Tablero extends JPanel {
     }
 
     //Recorrem les caselles que envolten a la que li hem posat una bomba i actualitzem el seu estat
-    public static void actualitzarCaselles(int i, int j) {
+    private static void actualitzarCaselles(int i, int j) {
         for (int k = dec(i); k <= inc(i); k++) {
             for (int l = dec(j); l <= inc(j); l++) {
                 if (k != i || l != j) {
                     //Incrementem el número de bombes (estat) de la casella ja que en té una més al costat
-                    tablero[k][l].estat = tablero[k][l].estat.sumarUn();
+                    tablero[k][l].incrementaEstat();
                 }
             }
         }
@@ -298,14 +242,14 @@ public class Tablero extends JPanel {
             for (int j = 0; j < DIM; j++) {
                 //Si el joc no s'ha acabat mostrem les caselles clicades, marcades i no clicades
                 if (!gameOver) {
-                    if (tablero[i][j].marcada) {
+                    if (tablero[i][j].isMarcada()) {
                         tablero[i][j].setImg(new ImageIcon("src/imagenes/11.png").getImage());
                     }
                     tablero[i][j].dibuja(g2d);
                 } else {    //Si el joc s'ha acabat mostrem el resultat final
                     //Les caselles marcades canvien la imatge segons si tenien bomba o no
-                    if (tablero[i][j].marcada) {
-                        if (tablero[i][j].estat == Estat.BOMBA) {
+                    if (tablero[i][j].isMarcada()) {
+                        if (tablero[i][j].getEstat() == Estat.BOMBA) {
                             tablero[i][j].setImg(new ImageIcon("src/imagenes/11.png").getImage());
                         } else {
                             tablero[i][j].setImg(new ImageIcon("src/imagenes/12.png").getImage());
@@ -313,12 +257,12 @@ public class Tablero extends JPanel {
                         tablero[i][j].dibuja(g2d);
                     } else { //Les no marcades
                         //Si tenen bomba les mostrem
-                        if (tablero[i][j].estat == Estat.BOMBA) {
+                        if (tablero[i][j].getEstat() == Estat.BOMBA) {
                             tablero[i][j].setImg(new ImageIcon("src/imagenes/9.png").getImage());
                             tablero[i][j].dibuja(g2d);
                         } else {
                             //I si no tenen bomba i estan clicades les mostrem, i sinó no
-                            if (tablero[i][j].clicada) {
+                            if (tablero[i][j].isClicada()) {
                                 tablero[i][j].dibuja(g2d);
                                 //tablero[i][j].dibuja(g2d, DIM + 1, tablero[i][j].buscaImagen());
                             } else {
@@ -366,16 +310,16 @@ public class Tablero extends JPanel {
     //Les caselles que envolten a la clicada s'han de tractar segons el seu estat
     private static void tractarCasella(int i, int j) {
         //Si està clicada, marcada, o conté una bomba no la tractarem
-        if (tablero[i][j].clicada || tablero[i][j].marcada || tablero[i][j].estat == Estat.BOMBA) {
+        if (tablero[i][j].isClicada() || tablero[i][j].isMarcada() || tablero[i][j].getEstat() == Estat.BOMBA) {
             return;
         }
         //Marquem la casella com a clicada i actualitzem el comptador
-        tablero[i][j].clicada = true;
+        tablero[i][j].setClicada(true);
         contClicadas++;
         //Li posem la imatge adequada per indicar el seu estat
         tablero[i][j].setImg(tablero[i][j].buscaImagen());
         //Si l'estat de la casella és CERO actualitzem el seus veïns recursivament 
-        if (tablero[i][j].estat == Estat.CERO) {
+        if (tablero[i][j].getEstat() == Estat.CERO) {
             actualitzarVeins(i, j);
         }
 
@@ -406,97 +350,6 @@ public class Tablero extends JPanel {
             if (j != DIM - 1) {
                 tractarCasella(inc(i), inc(j));
             }
-        }
-    }
-
-    //Classe que representa les caselles del taulell
-    class Casella extends Rectangle2D.Float {
-
-        private Graphics2D g2d;
-        private Image img;                  //imatge que posem a la casella
-        private float x;                    //coordenada x de la casella
-        private float y;                    //coordenada x de la casella
-        private Estat estat;                //estat en el que es troba la casella
-        private boolean clicada = false;    //per saber si l'han clicat
-        private boolean marcada = false;    //per saber si l'han marcat
-
-        //Creem la casella posant un requadre i l'estat a CERO
-        public Casella(float xpos, float ypos, float width, float height) {
-            x = xpos;
-            y = ypos;
-            setRect(x, y, width, height);
-            estat = Estat.CERO;
-        }
-
-        //Creem la casella assignant imatge, que fixa també el tamany    
-        public Casella(float x, float y, Image imagen) {
-            this(x, y, imagen.getWidth(null), imagen.getHeight(null));
-            img = imagen;
-        }
-
-        //Dibuixem la casella juntant la forma en la imatge que conté la seua propietat imatge
-        public void dibuja(Graphics2D g) {
-            g.fill(this);
-            g.drawImage(img, (int) x, (int) y, null);
-        }
-
-        //Retornem una nova casella en les mateixes propietats que la actual però situada unes caselles més avall 
-        public Casella copia(int numcas) {
-            return new Casella(this.x, this.y + img.getHeight(null) * numcas, this.img);
-        }
-
-        //Dibuixem la casella juntant la forma en la imatge que passem com a paràmetre
-        public void dibuja(Graphics2D g, int numcas, Image imagen) {
-            Casella copia = this.copia(numcas);
-            g.fill(copia);
-            g.drawImage(imagen, (int) copia.x, (int) copia.y, null);
-        }
-
-        //Indica si les coordenades x i y formen part de la casella
-        public boolean clicada(float x, float y) {
-            return getBounds2D().contains(x, y);
-        }
-
-        public void setImg(Image img) {
-            this.img = img;
-        }
-
-        //Retornem la imatge que representa l'estat de la casella
-        public Image buscaImagen() {
-            Image mshi = null;
-            switch (this.estat) {
-                case CERO:
-                    mshi = new ImageIcon("src/imagenes/0.png").getImage();
-                    break;
-                case UNO:
-                    mshi = new ImageIcon("src/imagenes/1.png").getImage();
-                    break;
-                case DOS:
-                    mshi = new ImageIcon("src/imagenes/2.png").getImage();
-                    break;
-                case TRES:
-                    mshi = new ImageIcon("src/imagenes/3.png").getImage();
-                    break;
-                case CUATRO:
-                    mshi = new ImageIcon("src/imagenes/4.png").getImage();
-                    break;
-                case CINCO:
-                    mshi = new ImageIcon("src/imagenes/5.png").getImage();
-                    break;
-                case SEIS:
-                    mshi = new ImageIcon("src/imagenes/6.png").getImage();
-                    break;
-                case SIETE:
-                    mshi = new ImageIcon("src/imagenes/7.png").getImage();
-                    break;
-                case OCHO:
-                    mshi = new ImageIcon("src/imagenes/8.png").getImage();
-                    break;
-                case BOMBA:
-                    mshi = new ImageIcon("src/imagenes/9.png").getImage();
-                    break;
-            }
-            return mshi;
         }
     }
 }
